039756c7094b60c672811973720d51c96388e50b
sanjay <sanjaykrishna.blubridge@evoplus.in>
2025-09-25 14:42:55 +0530

    Add c_cpp_properties.json and implement basic Tensor operations

--- practice.cpp
+++ practice.cpp
@@ -6,6 +6,11 @@ public:
     void hello() {
         cout << "Hello from Tensor!" << endl;
     }
+
+    void add() { cout << "Add function" << endl; }
+    void multiply() { cout << "Multiply function" << endl; }
+    void subtract() { cout << "Subtract function" << endl; }
+    void divide() { cout << "Divide function" << endl; }
 };
 
 int main() {
